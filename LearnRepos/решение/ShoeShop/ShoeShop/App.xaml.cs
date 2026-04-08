dde207a85f51411dddd31cf4a05dec7a8816c2ec
Nigami-light <mihailkuntasov380@gmail.com>
2026-04-08 20:17:31 +0300

    Added repos for learning demoexam from gheluag (спасибо легенда)

---
+++ LearnRepos/решение/ShoeShop/ShoeShop/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ShoeShop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+    }
+
+}
